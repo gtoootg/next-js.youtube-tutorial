c03fb22e2e54ddd547f93d4c6f3f7c8eecc7e4de
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-02-19 23:39:58 +0100

    update

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,9 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDatails(){
+  return(
+    <MeetupDetail image="https://i.gzn.jp/img/2018/01/15/google-gorilla-ban/00.jpg" title="first" address="some5" description="this is the first"/>
+  )
+}
+
+export default MeetupDatails
