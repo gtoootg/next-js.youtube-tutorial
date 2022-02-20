507d4e25be0ad15d48527a268496f4a3dcbda565
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-02-20 10:13:49 +0100

    update

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -2,8 +2,44 @@ import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDatails(){
   return(
-    <MeetupDetail image="https://i.gzn.jp/img/2018/01/15/google-gorilla-ban/00.jpg" title="first" address="some5" description="this is the first"/>
+    <MeetupDetail 
+      image="https://i.gzn.jp/img/2018/01/15/google-gorilla-ban/00.jpg"
+      title="first"
+      address="some5" 
+      description="this is the first"
+    />
   )
+}
+
+export async function getStaticPaths(){
+  return {
+    fallback:false ,
+    paths: [
+      { params: {
+        meetupId: 'm1',
+      }},
+    { params: {
+      meetupId: 'm2',
+    }},
+    ]
+  }
+}
+
+export async function getStaticProps(context){
+
+  const meetupId = context.params.meetupId
+
+  return{
+    props:{
+      meetupData:{
+        id: meetupId,
+        image:"https://i.gzn.jp/img/2018/01/15/google-gorilla-ban/00.jpg",title:"first",
+        address:"some5", 
+        description:"this is the first"
+      }
+      
+    }
+  }
 }
 
 export default MeetupDatails
